dc235a801afea55eeb150744ce40e0ea8254361f
A K M Mahdi <94472254+AKM-Mahdi@users.noreply.github.com>
2023-01-09 15:02:38 +0600

    string method

---
+++ Module-2/getline.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s); // to get string input using space
+
+    s.erase(s.begin());
+    s.pop_back();
+
+    cout<<s;
+
+
+
+}
+
---
+++ Module-2/string.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s="Mahdi";
+    //size of string
+    // cout<<s.size();
+
+    //remove the last elemenme from string
+    //s.pop_back();
+
+    //erase function
+    s.erase(s.begin());
+    cout<<s;
+}
